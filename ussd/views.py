dca574ed29a28e4e7480c48aa8af3efe005bf870
Adoday <mucyoadonis>
2021-12-14 01:31:43 +0200

    added

--- ussd/views.py
+++ ussd/views.py
@@ -35,6 +35,7 @@ def ussdApp(request):
             response += "1. igihumyo ni iki? \n"
             response += "2. Amoko y'ibihumyo bihingwa mu Rwanda \n"
             response += "3. Akamaro K'ibihumyo \n"
+            response += "4. ibigize igihumyo n'ko bahinga ibihumyo \n"
 
         elif text == '1*1':
             product="Menya igihumyo icyo aricyo"
@@ -48,9 +49,8 @@ def ussdApp(request):
             response += "1. Pleurote ( Soma Pulerote) \n"
             response += "2. Ganoderma ( soma Ganoderima) \n"
         elif text == '1*2*1':
-            response = "CON Pulerote  ni ubwoko bw'ibihumyo biribwa buhingirwa kugurishwa no kuribwa ku isi hose. Ubu bwoko bw'ibihumyo ntibugoye guteka kandi biraryoha. \n"
-        elif category =='1*2*1' and int(len(level)) == 5 and str(level[4]) in  str(level):
-            response = "END Murakoze gusura Mushtech \n"
+            response = "END Pulerote  ni ubwoko bw'ibihumyo biribwa buhingirwa kugurishwa no kuribwa ku isi hose. Ubu bwoko bw'ibihumyo ntibugoye guteka kandi biraryoha. \n"
+        
         elif text == '1*2*2':
             response ="END Ganoderima ni ubwoko bw'ibihumyo bukoreshwa nk'umuti mu buvuzi gakondo bwo ku mugabane wa Aziya. \n"
         
@@ -58,10 +58,23 @@ def ussdApp(request):
             response ="CON Ibihumyo bigira intungamubiri nyinshi cyane, zikubye 2 ugereranije n'inyama ndetse n'ibindi bihingwa duhereye ko bikize ku ntunga mubiri nka proteine, imyunyu ngugu n'amavitamine amwe n'amwe (B1,B2), nk'uko bigaragara mu mbonerahamwe ikurikira : \n"
             response += "1. Mu buzima \n"
             response += "2. Mu rwego rw'ubukungu \n"
+
         elif text == '1*3*1':
             response ="END Ibihumyo birinda indwara z'imirire mibi,Ibihumyo bituma igifu gikora neza;Ibihumyo byongera ubushobozi bw'umubiri mukwirwanaho, Ibihumyo birinda indwara zijyanye no kugira amaraso make \n"
         elif text == '1*3*2':
             response ="END Gutangira ntibisaba amafaranga menshi, kuko ibihumbi 20000 bishobora guhinga nibura m2 2 kandi ukaba ushobora gusarura umusaruro wagurisha kugeza ku mafaranga ibihumbi ijana na mirongo inani kugeza ku bihumbi magana abiri (180.000-200.000) , ibi rero bikaba byatuma n'abatishoboye babihinga. Ibihumyo ntibisaba ikorana buhanga rihambaye (rihanitse). Ni igihingwa ngenga bukungu kuko ku masoko asanzwe mu gihugu igiciro k'ibihumyo kiri hagati ya 1000 na 2000 frw ; Ibihumyo bifasha urubyiruko n'abagore kwihangira imirimo kandi bishobora kuba igisubizo cyo kurandura ubukene mu Rwanda. \n"
+        
+        elif text == '1*4':
+            response = "CON Dore ibigize igihumyo n'uko bahinga ibihumyo \n"
+            response += "1. Ibigize igihumyo \n"
+            response += "2. Uko bahinga ibihumyo \n"
+
+        elif text == '1*4*1':
+            response ="END Umurama : Ni uruvange rw ibyatsi biseye bongeramo umwayi bikamara iminsi 40 kugeza kuri 45 ngo umurama ube wakwiriye muri tube hose (ugeze igihe cyo guterwa) Dukoresha ibi bikurikira mu gukora umurama: urubingo; ibikatsi by'ibisheke; ibigorigori; ibishekesheke; ibikenyeri; ibishogoshogo by'ingano; by'ibishyimbo; by'umuceri, ibibarara by'ipamba ; ibishogoshogo by'uburo ( ibyo byose ubyiyambaje bishobora kuguha uruvange rw'umurama rwiza). Twifashisha ubwoko bw'ibyatsi bw'ibihingwa byinshi binyuranye kandi biboneka muri aka Karere kacu mu rwego rwo kurengera ibidukikije. \n"
+        elif text == '1*4*2':
+            response ="END Aho duhinga ibihumyo (igisharagati/ikibandahure)Mu buhinzi bw'ibihumyo singobwa kubaka inzu ikomeye, ihenze, isaba ibikoresho bihambaye cyangwa ubundi buhanga. ubuhehere, n'urunyurane rw'umwuka nibyo byitabwaho (bikenewe). Iyo umaze gukora igisharagati ucukuramo uturingoti dufite hagati ya cm 25 na 30 uzateramo imigina yawe, warangiza ugafata imigina (tubes) ugakuraho ya shashi no neho ugatereka mu butaka utondekanya ku murongo , warangiza ukorosaho agataka ka cm 1 noneho ukavomerera n'amazi. Iyo umaze kuvomerera ushyiraho isashe igondeye ku biti kugira ngo hagumemo ubuhehere kandi here kumagana kugeza igihe ibihumyo bizamuka .Iyo byatangiye kuzamuka,utwikurura ya sashe mbere ya saa moya za mugitondo mu gihe cy'igice cy'isaha igihe imvura itaguye, ukongera gutwikurura nanone igice cy'isaha izuba rirenze.Mu gihe cy'ubushuhe bwinshi ushobora gusuka amazi hahandi hateye byibura (300ml/umugina) buri kigoroba kugeza igihe ibihe bibaye byiza, kandi ukirinda gusuka amazi ku bihumyo bizamuka \n"
+        
+        
         else:
             response = "END Ukanze ibitaribyo, ongera mukanya"
         return HttpResponse(response)
